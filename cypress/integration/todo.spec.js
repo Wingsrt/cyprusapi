56d75d784cd1a6346198da06eb4548f76f9b7683
Wingsrt <squirrelstudio0@gmail.com>
2021-12-13 00:50:29 +0530

    first commit

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -15,5 +15,14 @@ describe("todo",function(){
         cy.get('input').type('a').should('have.value',"a");
 
         cy.get("button").click()
+
+        cy.intercept("GET","https://reqres.in/api/users/2",{username:",masai"})
+        
+
+        cy.request("https://reqres.in/api/users/2").then((data)=>{cy.log(data)
+        
+        //  expect(data.body.data.first_name)
+        
+        })
     })
 })
